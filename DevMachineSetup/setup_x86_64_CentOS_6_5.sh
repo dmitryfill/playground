c1f7b01467d770f148e17a103b163ecdeb3dc2ee
Dmitriy Filyushin <dmitry@apple.com>
2014-03-22 01:20:20 -0700

    adding few updates

--- DevMachineSetup/setup_x86_64_CentOS_6_5.sh
+++ DevMachineSetup/setup_x86_64_CentOS_6_5.sh
@@ -81,3 +81,9 @@ EOF
 
 chmod 755 ~/Desktop/idea.desktop
 
+# cloudera install
+cd ~/Downloads
+pwd
+wget http://archive.cloudera.com/cm5/installer/latest/cloudera-manager-installer.bin
+chmod u+x cloudera-manager-installer.bin
+# sudo ./cloudera-manager-installer.bin
